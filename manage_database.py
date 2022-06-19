2cf342a0bee33a0e77d68a5914c143949eab1144
Ivan Myshlyaev <ivanm1989@ya.ru>
2022-06-19 15:36:29 +0700

    added requirements.txt

--- manage_database.py
+++ manage_database.py
@@ -12,78 +12,85 @@ update_query = '''UPDATE orders SET id = %s, order_num = %s,
                                     cost_usd = %s, cost_rub = %s, delivery_date = %s 
                                 WHERE id = %s'''
 
-while True:
-    usd_rate = get_dollar_rate()  # current USD rate from http://www.cbr.ru/scripts/XML_daily.asp
 
-    table = get_data_from_sheets()
-    for row in table:
-        row.insert(3, row[2] * usd_rate)  # inserted cost in rubles
-        row[4] = datetime.strptime(row[4], '%d.%m.%Y').date()
-    print(table)
+def main():
+    while True:
+        usd_rate = get_dollar_rate()  # current USD rate from http://www.cbr.ru/scripts/XML_daily.asp
 
-    table_ids = set([i[0] for i in table])  # created set for faster membership tests
-    try:
-        connection = psycopg2.connect(
-            host=host,
-            user=user,
-            password=password,
-        )
-        connection.autocommit = True
+        table = get_data_from_sheets()
+        for row in table:
+            row.insert(3, row[2] * usd_rate)  # inserted cost in rubles
+            row[4] = datetime.strptime(row[4], '%d.%m.%Y').date()
+        print(table)
 
-        with connection.cursor() as cursor:
-            # checking existence of table "orders"
-            cursor.execute(f"select exists(SELECT * FROM information_schema.tables WHERE table_name=%s)", ('orders',))
-            if not cursor.fetchone()[0]:
-                # creating one if it doesn't exist
-                cursor.execute("""CREATE TABLE orders(
-                                id int,
-                                order_num int,
-                                cost_usd int,
-                                cost_rub real,
-                                delivery_date date)  
-                                """)
-                print('created table')
-            cursor.execute('SET datestyle TO "ISO, DMY";')  # changing table datestyle to dd/mm/yyyy format
+        table_ids = set([i[0] for i in table])  # created set for faster membership tests
+        try:
+            connection = psycopg2.connect(
+                host=host,
+                user=user,
+                password=password,
+            )
+            connection.autocommit = True
 
-            cursor.execute("SELECT * FROM orders LIMIT 1;")
-            if not cursor.fetchone():   # If the table is empty it is filled with data from table
-                for row in table:
-                    cursor.execute(insert_query, (*row, ))
+            with connection.cursor() as cursor:
+                # checking existence of table "orders"
+                cursor.execute(f'''select exists(SELECT * FROM information_schema.tables 
+                                                 WHERE table_name=%s)''', ('orders',))
+                if not cursor.fetchone()[0]:
+                    # creating one if it doesn't exist
+                    cursor.execute("""CREATE TABLE orders(
+                                    id int,
+                                    order_num int,
+                                    cost_usd int,
+                                    cost_rub real,
+                                    delivery_date date)  
+                                    """)
+                    print('created table')
+                cursor.execute('SET datestyle TO "ISO, DMY";')  # changing table datestyle to dd/mm/yyyy format
 
-            else:
-                cursor.execute('SELECT id FROM orders;')
-                database_ids = set([tp[0] for tp in cursor.fetchall()])
+                cursor.execute("SELECT * FROM orders LIMIT 1;")
+                if not cursor.fetchone():   # If the table is empty it is filled with data from table
+                    for row in table:
+                        cursor.execute(insert_query, (*row, ))
 
-                # searching ids to delete from DB
-                for _id in database_ids:
-                    if _id not in table_ids:
-                        cursor.execute(delete_query, (_id, ))
-                        print('deleted', _id)
+                else:
+                    cursor.execute('SELECT id FROM orders;')
+                    database_ids = set([tp[0] for tp in cursor.fetchall()])
 
-                # searching ids to add to DB
-                table_dict = {row[0]: row[1:] for row in table}
-                print(table_dict)
-                for _id in table_ids:
-                    if _id not in database_ids:
-                        cursor.execute(insert_query, (_id, *table_dict[_id],))
-                        print('inserted', _id)
+                    # searching ids to delete from DB
+                    for _id in database_ids:
+                        if _id not in table_ids:
+                            cursor.execute(delete_query, (_id, ))
+                            print('deleted', _id)
 
-                # creating and comparing sets of data from DB and from Google sheets
-                cursor.execute('SELECT id, order_num, cost_usd, delivery_date FROM orders;')
-                from_db = set(cursor.fetchall())
-                from_sheets = {(row[0], row[1], row[2], row[4]) for row in table}
+                    # searching ids to add to DB
+                    table_dict = {row[0]: row[1:] for row in table}
+                    print(table_dict)
+                    for _id in table_ids:
+                        if _id not in database_ids:
+                            cursor.execute(insert_query, (_id, *table_dict[_id],))
+                            print('inserted', _id)
 
-                difference = from_sheets - from_db
+                    # creating and comparing sets of data from DB and from Google sheets
+                    cursor.execute('SELECT id, order_num, cost_usd, delivery_date FROM orders;')
+                    from_db = set(cursor.fetchall())
+                    from_sheets = {(row[0], row[1], row[2], row[4]) for row in table}
 
-                # updating DB with changed rows
-                for element in difference:
-                    cursor.execute(update_query, (*element[:3], element[2]*usd_rate, element[3], element[0]))
+                    difference = from_sheets - from_db
 
-        time.sleep(3)   # sleep for 3 sec to avoid exceeding 'Read requests per minute per user'
+                    # updating DB with changed rows
+                    for element in difference:
+                        cursor.execute(update_query, (*element[:3], element[2]*usd_rate, element[3], element[0]))
 
-    # except Exception as exc:
-    #     print('[ERROR]', exc)
+            time.sleep(3)   # sleep for 3 sec to avoid exceeding 'Read requests per minute per user'
 
-    finally:
-        if connection:
-            connection.close()
+        except Exception as exc:
+            print('[ERROR]', exc)
+
+        finally:
+            if connection:
+                connection.close()
+
+
+if __name__ == '__main__':
+    main()
